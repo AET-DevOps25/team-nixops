3b8daebceff48846540c23a4eac5706998d7a32a
Philipp Grögel <31184695+GHOSCHT@users.noreply.github.com>
2025-05-30 09:03:34 +0000

    init(genai) (#9)

--- flake.nix
+++ flake.nix
@@ -5,29 +5,63 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     devenv.url = "github:cachix/devenv";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs = {
+        pyproject-nix.follows = "pyproject-nix";
+        uv2nix.follows = "uv2nix";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
-  outputs = inputs @ {
-    flake-parts,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs = inputs @ { flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         inputs.devenv.flakeModule
       ];
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
-      perSystem = {pkgs, ...}: {
-        packages.donna = pkgs.callPackage server/donna {};
-        devenv.shells.default = {
-          languages = {
-            kotlin.enable = true;
-            java = {
-              enable = true;
-              gradle.enable = true;
-              maven.enable = true;
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem =
+        { pkgs
+        , pyproject-nix
+        , uv2nix
+        , pyproject-build-systems
+        , ...
+        }:
+        let
+          genai = pkgs.callPackage ./genai {
+            inherit (inputs) pyproject-nix uv2nix pyproject-build-systems;
+          };
+        in
+        {
+          packages.donna = pkgs.callPackage server/donna { };
+          devShells.genai = genai.devShell;
+          packages.genai = genai.package;
+          devenv.shells.default = {
+            languages = {
+              kotlin.enable = true;
+              java = {
+                enable = true;
+                gradle.enable = true;
+                maven.enable = true;
+              };
+              python = {
+                uv.enable = true;
+              };
             };
           };
         };
-      };
     };
 }
---
+++ genai/src/__init__.py
@@ -0,0 +1,45 @@
+"""
+Module Name: GenAI server
+Description:
+    A basic FastAPI API that returns a "Hello, World!" message for the root endpoint.
+    This module demonstrates a minimal FastAPI setup and endpoint implementation.
+"""
+
+from fastapi import FastAPI
+import yaml
+
+from openapi_server.models.pet import Pet
+from typing import List
+
+app = FastAPI()
+
+
+@app.get(
+    "/pet/findByStatus",
+    responses={
+        200: {"model": List[Pet], "description": "successful operation"},
+        400: {"description": "Invalid status value"},
+    },
+    tags=["pet"],
+    summary="Finds Pets by status.",
+    response_model_by_alias=True,
+)
+async def find_pets_by_status() -> List[Pet]:
+    """Multiple status values can be provided with comma separated strings."""
+    pet_dict = {
+        "id": 1,
+        "name": "Frany",
+        "category": {"id": 10, "name": "Cats"},
+        "photoUrls": ["http://example.com/photo1.jpg", "http://example.com/photo2.jpg"],
+        "tags": [{"id": 101, "name": "cute"}, {"id": 102, "name": "small"}],
+        "status": "available",
+    }
+    return [Pet.from_dict(pet_dict)]
+
+
+def custom_openapi():
+    with open("openapi.yml", "r") as openapi:
+        return yaml.safe_load(openapi)
+
+
+app.openapi = custom_openapi
