610a58c1e4200a165fcb25bc847a69405a02b6dc
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2025-06-05 20:22:30 +0200

    add dummy postgres setup

---
+++ scraper/src/main/kotlin/com/nixops/scraper/model/User.kt
@@ -0,0 +1,14 @@
+package com.nixops.scraper.model
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "users")
+data class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column(nullable = false)
+    val name: String
+)
