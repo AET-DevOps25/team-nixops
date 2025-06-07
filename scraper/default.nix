e010eee0d5498473125ba5b77760c141faa4da8b
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2025-06-07 17:11:02 +0200

    add dockerImage to scraper

--- scraper/default.nix
+++ scraper/default.nix
@@ -1,16 +1,21 @@
 {
   lib,
-  stdenv,
+  pkgs,
   makeBinaryWrapper,
-  gradle,
   jre,
-  jdk21,
   gradle2nix,
+<<<<<<< HEAD
 }: let
+=======
+}:
+
+let
+
+>>>>>>> e64354e (add dockerImage to scraper)
   pname = "scraper";
   version = "0.0.1";
 
-  self = gradle2nix.builders.x86_64-linux.buildGradlePackage {
+  scraper = gradle2nix.builders.x86_64-linux.buildGradlePackage {
     inherit pname version;
 
     lockFile = ./gradle.lock;
@@ -28,6 +33,40 @@
       makeWrapper ${lib.getExe jre} $out/bin/${pname} \
         --add-flags "-jar $out/share/${pname}/${pname}-${version}.jar"
     '';
+
+    passthru = {
+      dockerImage = pkgs.dockerTools.buildImage {
+        name = "ghcr.io/aet-devops25/team-nixops-scraper";
+        tag = "latest";
+
+        copyToRoot = pkgs.buildEnv {
+          name = "image-root";
+          paths = [
+            (pkgs.runCommand "empty-tmp" { } ''
+              mkdir -p $out/tmp
+              chmod 1777 $out/tmp
+            '')
+          ];
+          pathsToLink = [ "/" ];
+        };
+
+        config = {
+          Cmd = [
+            "${placeholder "out"}/bin/${pname}"
+          ];
+          WorkingDir = "/";
+        };
+      };
+    };
   };
+
 in
+<<<<<<< HEAD
   self
+=======
+{
+  packages = {
+    inherit scraper;
+  };
+}
+>>>>>>> e64354e (add dockerImage to scraper)
