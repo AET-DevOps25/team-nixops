dedcb67b35caa7b17a9524223331913e6ba0098c
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2025-06-11 02:06:40 +0200

    first kapt based mapping between openapi and hibernate objects

--- scraper/src/main/kotlin/com/nixops/scraper/repository/UserRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.nixops.scraper.repository
-
-import com.nixops.scraper.model.User
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRepository : JpaRepository<User, Long>
