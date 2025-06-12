2010a17e9400dace250bfd75a3653ad407cbb1b5
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2025-06-12 16:25:37 +0200

    Create DB types and create beans for APIs

--- scraper/src/main/kotlin/com/nixops/scraper/tum_api/nat/model/NatProgram.kt
+++ scraper/src/main/kotlin/com/nixops/scraper/tum_api/nat/model/NatProgram.kt
@@ -5,13 +5,26 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class NatProgram(
-    @JsonProperty("study_id") val studyId: Int,
-    @JsonProperty("org_id") val orgId: Int,
-    @JsonProperty("school") val school: NatSchool,
-    @JsonProperty("spo_version") val spoVersion: String,
-    @JsonProperty("program_name") val programName: String,
-    @JsonProperty("degree") val degree: NatDegree,
-    @JsonProperty("degree_program_name") val degreeProgramName: String
+    @JsonProperty("study_id")
+    val studyId: Int,
+
+    @JsonProperty("org_id")
+    val orgId: Int,
+
+    @JsonProperty("school")
+    val school: NatSchool,
+
+    @JsonProperty("spo_version")
+    val spoVersion: String,
+
+    @JsonProperty("program_name")
+    val programName: String,
+
+    @JsonProperty("degree")
+    val degree: NatDegree,
+
+    @JsonProperty("degree_program_name")
+    val degreeProgramName: String
 )
 
 @JsonIgnoreProperties(ignoreUnknown = true)
