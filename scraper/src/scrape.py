33d58fe61ac20629f4408902d26a94e65319deb9
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2025-06-12 09:40:17 +0200

    extract termId from semester

--- scraper/src/scrape.py
+++ scraper/src/scrape.py
@@ -5,6 +5,7 @@ import requests_cache
 # API endpoint URL
 url = "https://api.srv.nat.tum.de"
 
+
 def get_org_rec(org_id, added=None):
     if added is None:
         added = set()
@@ -27,9 +28,10 @@ def get_org_rec(org_id, added=None):
 
     return org
 
+
 def get_all(*args, **kwargs):
     items = []
-    response = requests.get(*args, **kwargs).json()    
+    response = requests.get(*args, **kwargs).json()
     items += response["hits"]
 
     total = response["total_count"]
@@ -43,21 +45,32 @@ def get_all(*args, **kwargs):
         items += response["hits"]
     return items
 
+
 try:
-    requests_cache.install_cache("cache", expire_after=36000)
+    requests_cache.install_cache("cache", expire_after=None)
 
     # Make GET request
-    semester_key = requests.get(url + "/api/v1/semesters/lecture").json()[
-        "semester_key"
-    ]
-    print(f"semester: {semester_key}")
-    print()
-    
-    headers = {
-        'Accept': 'application/json'
-    }
+    semester = requests.get(url + "/api/v1/semesters/lecture").json()
 
-    curricula = requests.get("https://campus.tum.de/tumonline/ee/rest/slc.cm.cs.student/curricula/204", headers=headers).json()["resource"]
+    semester_key = semester["semester_key"]
+    semester_id = semester["semester_id_tumonline"]
+    semester_tag = semester["semester_tag"]
+    semester_title = semester["semester_title"]
+    
+    print("Semester:")
+    print("semester title:", semester_title)
+    print("semester tag:", semester_tag)
+    print("semester key:", semester_key)
+    print("semester id:", semester_id)
+    print()
+
+
+    headers = {"Accept": "application/json"}
+
+    curricula = requests.get(
+        "https://campus.tum.de/tumonline/ee/rest/slc.cm.cs.student/curricula/204",
+        headers=headers,
+    ).json()["resource"]
 
     # query = input("Enter Study Program: ")
     # spo = input("Enter Id: ")
@@ -65,12 +78,8 @@ try:
     query = "M.Sc. Informatik"
     spo = "20231"
 
-    for program in get_all(
-        url + "/api/v1/programs/search",
-        params = {
-            "q": query
-        }
-    ):
+    print("Study Program:")
+    for program in get_all(url + "/api/v1/programs/search", params={"q": query}):
         print(f"program: {program['degree_program_name']}")
 
         study_id = program["study_id"]
@@ -90,7 +99,7 @@ try:
             print()
             continue
 
-        long_name = f"{program['program_name']} [{spo_version}], {program["degree"]["degree_type_name"]}"
+        long_name = f"{program['program_name']} [{spo_version}], {program['degree']['degree_type_name']}"
         print("long name:", long_name)
 
         found_id = False
@@ -108,56 +117,35 @@ try:
             print("no curriculum available")
             print()
             continue
-        
+
         print("curriculum id:", curriculum_id)
         print()
 
-    courses = []
-    
-    term_id = 204
-
-    params = {
-        "$filter": f"courseNormKey-eq=LVEAB;curriculumVersionId-eq={curriculum_id};orgId-eq=1;termId-eq={term_id}",
-        # "$orderBy": "title asc",  # Corrected orderby parameter
-        "$skip": 0,
-        "$top": 20
-    }
-    
-    response = requests.get("https://campus.tum.de/tumonline/ee/rest/slc.tm.cp/student/courses", params=params, headers=headers).json()
-    courses += response["courses"]
-    
-    print(response["totalCount"])
-
-    while len(courses) < response["totalCount"]:
-        params = {
-            "$filter": f"courseNormKey-eq=LVEAB;curriculumVersionId-eq={curriculum_id};orgId-eq=1;termId-eq={term_id}",
-            # "$orderBy": "title asc",  # Corrected orderby parameter
-            "$skip": len(courses),
-            "$top": len(courses)+20
-        }
-    
-        response = requests.get("https://campus.tum.de/tumonline/ee/rest/slc.tm.cp/student/courses", params=params, headers=headers).json()
-        courses += response["courses"]
-        
-        print(len(courses), response["totalCount"])
-
+    print("Fetch Modules:")
     modules = []
-    response = requests.get(url + "/api/v1/mhb/module", params={"org_id": school_org_id}).json()
+    response = requests.get(
+        url + "/api/v1/mhb/module", params={"org_id": school_org_id}
+    ).json()
     modules += response["hits"]
 
     total = response["total_count"]
     while "next_offset" in response and response["next_offset"] is not None:
         print(f"{len(modules)}/{response['total_count']}")
-        response = requests.get(url + "/api/v1/mhb/module", params={"org_id": school_org_id, "offset": response["next_offset"]}).json()
+        response = requests.get(
+            url + "/api/v1/mhb/module",
+            params={"org_id": school_org_id, "offset": response["next_offset"]},
+        ).json()
         modules += response["hits"]
+    print()
 
+    print("Modules:")
     extra_module_mapping = {}
     for module in modules:
         module_id = module["module_id"]
         module_title = module["module_title"]
         module_code = module["module_code"]
 
-        print(module_id, module_title, module_code)
+        # print(module_id, module_title, module_code)
 
         module = requests.get(url + f"/api/v1/mhb/module/{module_code}").json()
 
@@ -165,13 +153,48 @@ try:
         if semester_key in module["courses"]:
             for course in module["courses"][semester_key]:
                 course_id = course["course_id"]
-                print(">>>", course_id)
+                # print(">>>", course_id)
 
                 if course_id not in extra_module_mapping:
                     extra_module_mapping[course_id] = []
-                    
+
                 extra_module_mapping[course_id].append(module_id)
-            
+    print()
+
+    print("Fetch Courses:")
+    courses = []
+
+    params = {
+        "$filter": f"courseNormKey-eq=LVEAB;curriculumVersionId-eq={curriculum_id};orgId-eq=1;termId-eq={semester_id}",
+        # "$orderBy": "title asc",  # Corrected orderby parameter
+        "$skip": 0,
+        "$top": 20,
+    }
+    response = requests.get(
+        "https://campus.tum.de/tumonline/ee/rest/slc.tm.cp/student/courses",
+        params=params,
+        headers=headers,
+    ).json()
+    courses += response["courses"]
+
+    while len(courses) < response["totalCount"]:
+        params = {
+            "$filter": f"courseNormKey-eq=LVEAB;curriculumVersionId-eq={curriculum_id};orgId-eq=1;termId-eq={semester_id}",
+            # "$orderBy": "title asc",  # Corrected orderby parameter
+            "$skip": len(courses),
+            "$top": len(courses) + 20,
+        }
+
+        response = requests.get(
+            "https://campus.tum.de/tumonline/ee/rest/slc.tm.cp/student/courses",
+            params=params,
+            headers=headers,
+        ).json()
+        courses += response["courses"]
+        print(len(courses), response["totalCount"])
+    print()
+
+    print("Courses:")
     for course in courses:
         # print(json.dumps(course, indent=4))
         # print(list(course.keys()))
@@ -179,13 +202,12 @@ try:
         # print(course["semesterDto"])
         new_course = requests.get(url + f"/api/v1/course/{course_id}").json()
 
-    
         # print(course_id, course["courseTitle"]["value"])
 
         # if "Operations Research" in course["courseTitle"]["value"]:
-        
-            # print(json.dumps(course, indent=4))
-            # print(json.dumps(new_course, indent=4))
+
+        # print(json.dumps(course, indent=4))
+        # print(json.dumps(new_course, indent=4))
 
         if new_course["modules"] != []:
             pass
@@ -197,7 +219,7 @@ try:
             print("no module :(")
             print(course_id, course["courseTitle"]["value"])
 
-
+            print("org_id", org_id, new_course["org"]["org_id"])
 
     # response = requests.get("https://campus.tum.de/tumonline/ee/rest/slc.tm.cp/student/modules", headers=headers)
     # print(response.text)
