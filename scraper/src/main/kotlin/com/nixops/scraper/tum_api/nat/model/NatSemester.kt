6d1adfc0bcb90d0c840f7f6e5846350598de08d3
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2025-06-25 12:54:06 +0200

    continue migrating to exposed

--- scraper/src/main/kotlin/com/nixops/scraper/tum_api/nat/model/NatSemester.kt
+++ scraper/src/main/kotlin/com/nixops/scraper/tum_api/nat/model/NatSemester.kt
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class NatSemester(
     @JsonProperty("semester_key") val semesterKey: String,
-    @JsonProperty("semester_id_tumonline") val semesterIdTumOnline: Int?,
+    @JsonProperty("semester_id_tumonline") val semesterIdTumOnline: Int,
     @JsonProperty("semester_tag") val semesterTag: String,
     @JsonProperty("semester_title") val semesterTitle: String
 )
