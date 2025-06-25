ed585a30a0ca7d256d6f7a063c901971fa482f0b
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2025-06-25 14:09:50 +0200

    remove old test code

--- scraper/src/main/kotlin/com/nixops/scraper/model/Users.kt
+++
@@ -1,9 +0,0 @@
-package com.nixops.scraper.model
-
-import org.jetbrains.exposed.sql.Table
-
-object Users : Table() {
-  val id = integer("id").autoIncrement()
-  val name = varchar("name", length = 50)
-  override val primaryKey = PrimaryKey(id)
-}
