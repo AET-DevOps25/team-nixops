aa6588e139bb1e048b474a6101a88da24d940f44
GHOSCHT <31184695+GHOSCHT@users.noreply.github.com>
2025-06-27 00:48:00 +0200

    Add nix docker packaging

--- client/flake.nix
+++ client/flake.nix
@@ -4,15 +4,18 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    ,
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {inherit system;};
-      in {
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
         devShells = with pkgs; {
           default = mkShell {
             buildInputs = [
@@ -32,18 +35,56 @@
           };
         };
 
-        packages = with pkgs; {
-          default = buildNpmPackage {
-            pname = "nixops-client";
+        packages = with pkgs; let
+          pname = "client";
+          drv = buildNpmPackage {
+            inherit pname;
             version = "0.0.1";
+            meta.mainProgram = pname;
             src = ./.;
 
             npmDeps = importNpmLock {
               npmRoot = ./.;
             };
 
+            postInstall = ''
+              mkdir -p $out/bin
+              exe="$out/bin/${pname}"
+              lib="$out/lib/node_modules/${pname}/.next"
+              cp -r ./.next $lib
+              touch $exe
+              chmod +x $exe
+              echo "
+                  #!/usr/bin/env bash
+                  cd $lib/..
+                  ${pkgs.nodePackages_latest.pnpm}/bin/pnpm run start" > $exe
+            '';
+
             npmConfigHook = importNpmLock.npmConfigHook;
           };
+        in
+        {
+          dockerImage =
+            pkgs.dockerTools.buildLayeredImage
+              {
+                name = "client";
+                tag = "latest";
+                config = {
+                  Cmd = [
+                    "${lib.getExe pkgs.bash}"
+                    "-c"
+                    "${lib.getExe drv}"
+                  ];
+                  Env = [
+                    "PATH=/bin/"
+                  ];
+                  ExposedPorts = {
+                    "3000/tcp" = { };
+                  };
+                };
+                contents = [ pkgs.coreutils pkgs.util-linux pkgs.bash drv ];
+              };
+          default = drv;
         };
       }
     );
