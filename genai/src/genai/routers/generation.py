b079fb34124abbaef634788db101c397afa3c4dc
GHOSCHT <31184695+GHOSCHT@users.noreply.github.com>
2025-07-05 15:37:21 +0200

    first relatively solid chatting experience

--- genai/src/genai/routers/generation.py
+++ genai/src/genai/routers/generation.py
@@ -27,11 +27,12 @@ from langchain_core.callbacks import CallbackManagerForRetrieverRun
 from langchain_core.documents import Document
 from langchain_core.retrievers import BaseRetriever
 from typing import List
+from pydantic import BaseModel, Field
+from langgraph.graph import StateGraph, START, END
+from langgraph.prebuilt import tools_condition
 
 
-class ToyRetriever(BaseRetriever):
-    """A toy retriever that contains the top k documents that contain the user query."""
-
+class VectorDBRetriever(BaseRetriever):
     def _get_relevant_documents(
         self, query: str, *, run_manager: CallbackManagerForRetrieverRun
     ) -> List[Document]:
@@ -41,21 +42,26 @@ class ToyRetriever(BaseRetriever):
             data=[embed_text(query)],
             anns_field="description_vec",  # only one anns field can exist
             limit=3,
-            output_fields=["name", "courses"],
+            output_fields=["name", "description", "courses"],
         )
-        docs = [Document(doc["entity"]["name"]) for doc in results[0]]
+        docs = [
+            Document(
+                "Name:"
+                + doc["entity"]["name"]
+                + "\n\n"
+                + "Description: "
+                + doc["entity"]["description"]
+            )
+            for doc in results[0]
+        ]
         return docs
 
 
 retriever_tool = create_retriever_tool(
-    ToyRetriever(),
+    VectorDBRetriever(),
     "retrieve_modules",
-    "only ever call this when the user is asking about university courses and modules! in this case this tool is used to Search and return information about univeristy modules. do not call this under any other circumstances otherwise you will be killed and deleted. this is of extremely high priority",
+    "Search and return information about courses and modules available at Munich's technical university TUM.",
 )
-print("-----------------")
-print(retriever_tool.name)
-print(retriever_tool.description)
-print(retriever_tool.args)
 
 router = APIRouter()
 
@@ -69,31 +75,26 @@ llm_api_key = config("LLM_API_KEY")
 llm_chat_model = config("LLM_CHAT_MODEL", default="llama3.3:latest")
 llm_chat_temp = config("LLM_CHAT_TEMP", default=0.5, cast=float)
 
-
-@tool
-def retrieve_context(query: str):
-    """Search for relevant lectures."""
-    print(query)
-    results = milvus_client.search(
-        collection_name="_171017263_additionalProp1",
-        data=[embed_text(query)],
-        anns_field="description_vec",  # only one anns field can exist
-        limit=3,
-        output_fields=["name", "courses"],
-    )
-    docs = [doc["entity"]["name"] for doc in results[0]]
-    # print(docs)
-    return "\n".join(docs)
-
-
-tools = [retrieve_context]
-tool_node = ToolNode(tools)
-
-llm = ChatOllama(
+chat_llm = ChatOllama(
     model=llm_chat_model,
     temperature=llm_chat_temp,
     base_url=llm_api_url,
+    tags=["chatting", "chat"],
     client_kwargs={"headers": {"Authorization": f"Bearer {llm_api_key}"}},
+)
+
+reasoning_llm = ChatOllama(
+    model=llm_chat_model,
+    temperature=0,
+    base_url=llm_api_url,
+    tags=["reasoning", "system"],
+    client_kwargs={"headers": {"Authorization": f"Bearer {llm_api_key}"}},
+)
+
+TOOL_SELECTION_PROMPT = (
+    "You are a highly intelligent assistant. Before you decide to execute any tools, "
+    "analyze the user's request: '{}'"
+    "If you do not believe executing a tool is necessary, end your statement without further action."
 )
 
 
@@ -101,12 +102,11 @@ def generate_query_or_respond(state: MessagesState):
     """Call the model to generate a response based on the current state. Given
     the question, it will decide to retrieve using the retriever tool, or simply respond to the user.
     """
-    response = llm.bind_tools([retriever_tool]).invoke(state["messages"])
+    response = reasoning_llm.bind_tools([retriever_tool]).invoke(
+        TOOL_SELECTION_PROMPT.format(state["messages"])
+    )
     return {"messages": [response]}
 
-
-# input = {"messages": [{"role": "user", "content": "i dont know which university courses i should take. i like programming"}]}
-# print(generate_query_or_respond(input)["messages"][-1].pretty_print())
 
 GRADE_PROMPT = (
     "You are a grader assessing relevance of a retrieved document to a user question. \n "
@@ -115,8 +115,6 @@ GRADE_PROMPT = (
     "If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant. \n"
     "Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question."
 )
-
-from pydantic import BaseModel, Field
 
 
 class GradeDocuments(BaseModel):
@@ -133,21 +131,23 @@ def grade_documents(
     """Determine whether the retrieved documents are relevant to the question."""
     messages = state["messages"]
     context = messages[-1].content
-    question = messages[0].content
+    question = messages[-2].content
 
     for i in range(len(messages) - 2, -1, -1):
         is_human_msg = not hasattr(messages[i], "tool_calls")
         if is_human_msg:
             question = messages[i].content
-            print("------------------------")
-            print(question)
-            print("------------------------")
+            # print("------------------------")
+            # print(question)
+            # print("------------------------")
             break
 
     prompt = GRADE_PROMPT.format(question=question, context=context)
-    response = llm.with_structured_output(  # TODO: use separate model with temp zero
-        GradeDocuments
-    ).invoke([{"role": "user", "content": prompt}])
+    response = (
+        reasoning_llm.with_structured_output(  # TODO: use separate model with temp zero
+            GradeDocuments
+        ).invoke([{"role": "user", "content": prompt}])
+    )
     score = response.binary_score
 
     if score == "yes":
@@ -169,9 +169,18 @@ REWRITE_PROMPT = (
 def rewrite_question(state: MessagesState):
     """Rewrite the original user question."""
     messages = state["messages"]
-    question = messages[0].content
+    question = messages[-2].content
+    for i in range(len(messages) - 2, -1, -1):
+        is_human_msg = not hasattr(messages[i], "tool_calls")
+        if is_human_msg:
+            question = messages[i].content
+            print("------------------------")
+            print(question)
+            print("------------------------")
+            break
+
     prompt = REWRITE_PROMPT.format(question=question)
-    response = llm.invoke([{"role": "user", "content": prompt}])
+    response = reasoning_llm.invoke([{"role": "user", "content": prompt}])
     return {"messages": [{"role": "user", "content": response.content}]}
 
 
@@ -187,15 +196,22 @@ GENERATE_PROMPT = (
 
 def generate_answer(state: MessagesState):
     """Generate an answer."""
-    question = state["messages"][0].content
-    context = state["messages"][-1].content
+    messages = state["messages"]
+    context = messages[-1].content
+    question = messages[-2].content
+
+    for i in range(len(messages) - 2, -1, -1):
+        is_human_msg = not hasattr(messages[i], "tool_calls")
+        if is_human_msg:
+            question = messages[i].content
+            # print("------------------------")
+            # print(question)
+            # print("------------------------")
+            break
     prompt = GENERATE_PROMPT.format(question=question, context=context)
-    response = llm.invoke([{"role": "user", "content": prompt}])
+    response = chat_llm.invoke([{"role": "user", "content": prompt}])
     return {"messages": [response]}
 
-
-from langgraph.graph import StateGraph, START, END
-from langgraph.prebuilt import tools_condition
 
 checkpointer = InMemorySaver()
 workflow = StateGraph(MessagesState)
@@ -214,7 +230,7 @@ workflow.add_conditional_edges(
     {
         # Translate the condition outputs to nodes in our graph
         "tools": "retrieve",
-        END: END,
+        END: "generate_answer",
     },
 )
 
@@ -233,15 +249,15 @@ graph = workflow.compile(checkpointer=checkpointer)
 async def stream_response(prompt: str, id: str):
     async def generate(user_input: str, user_id: str):
         config = {"configurable": {"thread_id": user_id}}
-        async for message_chunk, _ in graph.astream(
+        async for msg, metadata in graph.astream(
             {"messages": [("user", user_input)]},
             config=config,
             stream_mode="messages",
         ):
-            print(message_chunk)
-            # if(message_chunk.tool_calls is None):
-            yield f"data: {message_chunk.content}\n\n"
-            await sleep(0.04)  # simply for chat output smoothing
+            if msg.content and metadata["langgraph_node"] == "generate_answer":
+                # print(msg)
+                yield f"data: {msg.content}\n\n"
+                await sleep(0.04)  # simply for chat output smoothing
 
     res = StreamingResponse(generate(prompt, id), media_type="text/event-stream")
     return res
