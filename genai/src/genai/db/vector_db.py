b748a255306c72f8f4a79fcc8aa0e4ed34162ac9
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2025-07-10 19:23:43 +0200

    make /embed queue a background task and exit immediately

--- genai/src/genai/db/vector_db.py
+++ genai/src/genai/db/vector_db.py
@@ -36,7 +36,7 @@ schema = MilvusClient.create_schema(
 )
 # each row is a module
 schema.add_field(field_name="id", datatype=DataType.INT64, is_primary=True)
-schema.add_field(field_name="name", datatype=DataType.VARCHAR, max_length=128)
+schema.add_field(field_name="name", datatype=DataType.VARCHAR, max_length=256)
 schema.add_field(field_name="description", datatype=DataType.VARCHAR, max_length=16192)
 schema.add_field(
     field_name="description_vec", datatype=DataType.FLOAT_VECTOR, dim=embedding_len()
