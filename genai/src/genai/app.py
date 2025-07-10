5a08bc7186c29e06fc1dc6a4efd2782dbff3bf6f
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2025-07-10 19:33:08 +0200

    fix genai nix build

--- genai/src/genai/app.py
+++ genai/src/genai/app.py
@@ -49,5 +49,9 @@ app.openapi = custom_openapi
 
 def run():
     uvicorn.run(
-        app, host="0.0.0.0", log_level="info", log_config=LOGGING_CONFIG, workers=16
+        "genai.app:app",
+        host="0.0.0.0",
+        log_level="info",
+        log_config=LOGGING_CONFIG,
+        workers=16,
     )
