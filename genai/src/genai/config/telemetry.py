edfd4ea8d421df6cf49c62251ba0ad4f71ee889a
GHOSCHT <31184695+GHOSCHT@users.noreply.github.com>
2025-07-17 22:03:37 +0200

    add histogram

--- genai/src/genai/config/telemetry.py
+++ genai/src/genai/config/telemetry.py
@@ -1,4 +1,4 @@
-from logging import info
+import time
 from fastapi import FastAPI, Request
 from opentelemetry import metrics
 from opentelemetry.sdk.metrics import MeterProvider
@@ -8,6 +8,8 @@ import importlib.metadata
 from prometheus_client import CollectorRegistry, Counter, multiprocess, make_asgi_app
 
 import os
+
+from prometheus_client.metrics import Histogram
 
 os.environ["PROMETHEUS_MULTIPROC_DIR"] = "/tmp/metrics"
 os.makedirs("/tmp/metrics", exist_ok=True)
@@ -23,19 +25,39 @@ def init_telemetry(app: FastAPI):
 
     http_requests_total = Counter(
         "http_requests_total",
-        "Counts of HTTP requests by method and path",
+        "Total HTTP requests",
+        ["method", "path", "status_code"],
+    )
+    REQUEST_LATENCY = Histogram(
+        "http_request_duration_seconds",
+        "HTTP request duration in seconds",
         ["method", "path"],
-        registry=registry,
+        buckets=[0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10],
     )
 
     @app.middleware("http")
-    async def prometheus_request_counter(request: Request, call_next):
-        path = request.url.path
+    async def prometheus_middleware(request: Request, call_next):
         method = request.method
+        path = request.url.path
 
-        http_requests_total.labels(method=method, path=path).inc()
+        start_ts = time.time()
+        try:
+            response = await call_next(request)
+            status_code = response.status_code
+        except Exception:
+            status_code = 500
+            raise
+        finally:
+            duration = time.time() - start_ts
+            # Observe the duration in seconds
+            REQUEST_LATENCY.labels(method=method, path=path).observe(duration)
+            # Increment the counter
+            http_requests_total.labels(
+                method=method,
+                path=path,
+                status_code=str(status_code),
+            ).inc()
 
-        response = await call_next(request)
         return response
 
     global genai_release_version
