95411d3d857eac5c1784961bd1c8e26768ab2402
Philipp Grögel <31184695+GHOSCHT@users.noreply.github.com>
2025-07-19 19:56:52 +0000

    fix: bundle openapi spec in python module (#103)
    * bundle openapi spec in python module
    
    * Add genai docs to readme

--- genai/src/genai/config/openapi.py
+++ genai/src/genai/config/openapi.py
@@ -1,30 +1,18 @@
 import yaml
+from importlib import resources
+from datetime import date, datetime
+from fastapi.encoders import jsonable_encoder
 
 
 def custom_openapi():
-    with open("openapi.yml", "r") as genai_file:
-        with open("../scraper/openapi.yaml", "r") as scraper_file:
-            if genai_file is not None and scraper_file is not None:
-                genai_openapi = yaml.safe_load(genai_file)
-                scraper_openapi = yaml.safe_load(scraper_file)
-                genai_openapi["components"]["schemas"]["Semester"] = scraper_openapi[
-                    "components"
-                ]["schemas"]["Semester"]
-                genai_openapi["components"]["schemas"]["StudyProgram"] = (
-                    scraper_openapi["components"]["schemas"]["StudyProgram"]
-                )
-                genai_openapi["components"]["schemas"]["Module"] = scraper_openapi[
-                    "components"
-                ]["schemas"]["Module"]
-                genai_openapi["components"]["schemas"]["ModuleCourses"] = (
-                    scraper_openapi["components"]["schemas"]["ModuleCourses"]
-                )
-                genai_openapi["components"]["schemas"]["Course"] = scraper_openapi[
-                    "components"
-                ]["schemas"]["Course"]
-                genai_openapi["components"]["schemas"]["Appointment"] = scraper_openapi[
-                    "components"
-                ]["schemas"]["Appointment"]
-                return genai_openapi
-            else:
-                return {}
+    schema = yaml.safe_load(resources.read_text("genai", "openapi.yml"))
+
+    schema = jsonable_encoder(
+        schema,
+        custom_encoder={
+            datetime: lambda dt: dt.isoformat(),
+            date: lambda d: d.isoformat(),
+        },
+    )
+
+    return schema
